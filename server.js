80c351f7dd27028bb1a063a7c609a3623af3e40a
virajbhanderi2 <virajbhanderi777@gmail.com>
2024-10-07 14:31:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const cors = require("cors");
+
+const connectDB = require('./config/db');
+const transactions = require('./routes/transactions');
+
+dotenv.config({ path: './config/config.env' });
+connectDB();
+const PORT = process.env.PORT || '5000'
+const app = express()
+app.use(express.json());
+app.use(cors())
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('common'));
+}
+
+app.use('/api/v1/transactions', transactions);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+})
